83c567f421c8947c4546495e57992cf9e0793f70
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-05-20 20:12:39 -0700

    resources seed

--- data/seeds/02-make-projects.js
+++ data/seeds/02-make-projects.js
@@ -20,3 +20,27 @@ const projects = [
         project_completed: true
       },
 ]
+
+const resources = [
+      { resource_name: "Stack Overflow" },
+      { resource_name: "The Hub", resource_description: "BloomTech resource for learners" },
+      { resource_name: "Youtube" },
+      { resource_name: "Canvas", resource_description: "Warm up materials for daily projects" },
+      { resource_name: "Solution video", resource_description: "Sage advice from Gabe found in solution video" },
+
+]
+
+const tasks = [
+
+]
+
+const project_resources = [
+
+]
+
+exports.seed = async function(knex){
+    await knex("projects").insert(projects)
+    await knex("resources").insert(resources)
+    await knex("tasks").insert(tasks)
+    await knex("project_resources").insert(project_resources)
+}
