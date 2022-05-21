1a48b7e485edce96573648fbd6f4de6ba179fb75
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-05-20 18:05:48 -0700

    projects table

--- data/migrations/20220521004218_sprint-migrations.js
+++ data/migrations/20220521004218_sprint-migrations.js
@@ -6,15 +6,20 @@ exports.up = async function(knex) {
   await knex.schema
 
     .createTable("projects", tbl => {
-        tbl.increments()
+        tbl.increments("project_id")
+        tbl.string("project_name", 35)
+            .notNullable()
+        tbl.string("project_description", 150)
+        tbl.boolean("project_completed")
+            .defaultTo("false")
     })
     
     .createTable("resources", tbl => {
-        tbl.increments()
+        tbl.increments("resource_id")
     })
     
     .createTable("tasks", tbl => {
-        tbl.increments()
+        tbl.increments("task_id")
     })
 
     .createTable("project_resources", tbl => {
