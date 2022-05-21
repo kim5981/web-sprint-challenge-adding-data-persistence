e1c9c8d1e53fad5d69fb08bb0f41b9a74abb5464
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-05-20 17:50:57 -0700

    migrations skeleton

--- api/server.js
+++ api/server.js
@@ -1,12 +1,28 @@
-// build your server here and require it from index.js
 const express = require("express")
+
+const projectsRouter = require("./project/router")
+const resourcesRouter = require("./resource/router")
+const tasksRouter = require("./task/router")
 
 const server = express()
 
 server.use(express.json())
 
+server.use("/api/projects", projectsRouter)
+server.use("/api/resources", resourcesRouter)
+server.use("/api/tasks", tasksRouter)
+
 server.use("*", (req, res) => {
     res.json({ message: "server running, over"})
 })
+
+server.use((err, req, res, next) => { //eslint-disable-line
+    res.status(err.status || 500).json({
+        customMessage: "yikes",
+        message: err.message,
+        stack: err.stack
+    })
+})
+
 
 module.exports = server
--- api/task/router.js
+++ api/task/router.js
@@ -3,20 +3,13 @@ const router = require("express").Router()
 
 const Task = require("./model")
 
-router.get("/api/tasks", (req, res, next) => {
+router.get("/", (req, res, next) => {
     Task.getTasks()
-    .then(() => {
+    .then(task => {
         res.json("getting tasks")
     })
     .catch(next)
 })
 
-router.use((err, req, res, next) => { //eslint-disable-line
-    res.status(err.status || 500).json({
-        customMessage: "yikes",
-        message: err.message,
-        stack: err.stack
-    })
-})
 
 module.exports = router
