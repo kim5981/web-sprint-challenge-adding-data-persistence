b6370e701e5ad20aaa4da466d6169c9e17eb1871
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-05-20 19:55:07 -0700

    projects seed

--- data/migrations/20220521004218_sprint-migrations.js
+++ data/migrations/20220521004218_sprint-migrations.js
@@ -39,7 +39,21 @@ exports.up = async function(knex) {
     })
 
     .createTable("project_resources", tbl => {
-        tbl.increments()
+        tbl.increments("project_resource_id")
+        tbl.integer("project_id")
+            .unsigned()
+            .notNullable()
+            .references("project_id")
+            .inTable("projects")
+            .onDelete("RESTRICT")
+            .onUpdate("RESTRICT")
+        tbl.integer("resource_id")
+            .unsigned()
+            .notNullable()
+            .references("resource_id")
+            .inTable("resources")
+            .onDelete("RESTRICT")
+            .onUpdate("RESTRICT")
     })
 };
 
---
+++ data/seeds/02-make-projects.js
@@ -0,0 +1,22 @@
+const projects = [
+    { 
+      project_name: "Module 1 Project",
+      project_description: "Intro to Relational Databases",
+      project_completed: true
+    },
+    { 
+        project_name: "Module 2 Project",
+        project_description: "Database Schema Design",
+        project_completed: true
+      },
+      { 
+        project_name: "Module 3 Project",
+        project_description: "Multi-Table Queries",
+        project_completed: true
+      },
+      { 
+        project_name: "Module 4 Project",
+        project_description: "Data Modeling",
+        project_completed: true
+      },
+]
